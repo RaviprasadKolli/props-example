f7b893fcf78c16a5a0a391ae3e192326b3d4caff
RaviprasadKolli <git@stackblitz.com>
2024-10-14 23:22:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropertiesExapmple from './PropertiesExapmple';
+
+function App() {
+  return (
+    <>
+      <PropertiesExapmple name="Naveen" />
+      <PropertiesExapmple name="Ravi" />
+      <PropertiesExapmple name="Anil" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/PropertiesExapmple.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PropertiesExapmple = (props) => {
+  return (
+    <>
+      <h1>Hello {props.name}</h1>
+    </>
+  );
+};
+export default PropertiesExapmple;
